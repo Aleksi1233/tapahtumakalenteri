9f3d770157d0642723da872b103e3bea86af73f0
Niskanen Aleksi M S <aleniska@cubbli22-gold.local>
2025-02-04 16:04:32 +0200

    Updated schema.sql, to have the events -table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE events (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    description TEXT,
+    event_start TEXT NOT NULL,
+    event_end TEXT NOT NULL,
+    event_space TEXT
+, event_type TEXT, username TEXT
+);
